29036492199c4a81128e3e15e896d614839025b5
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2016-07-21 15:31:30 -0700

    CIRCUIT - Refactored AemToSolr to use a main wrapper.

--- camel-aem-to-solr/src/main/java/com/gastongonzalez/circuit/camel/aem/main/AemToSolrRoutes.java
+++ camel-aem-to-solr/src/main/java/com/gastongonzalez/circuit/camel/aem/main/AemToSolrRoutes.java
@@ -1,6 +1,5 @@
 package com.gastongonzalez.circuit.camel.aem.main;
 
-import com.gastongonzalez.circuit.camel.aem.IndexerConstants;
 import com.gastongonzalez.circuit.camel.aem.PropertiesHelper;
 import com.gastongonzalez.circuit.camel.aem.processor.JmsDocToSolrDoc;
 import org.apache.activemq.camel.component.ActiveMQComponent;
@@ -8,71 +7,84 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.component.solr.SolrConstants;
-import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
 
 /**
  * Camel route definitions for reading AEM index requests off an ActiveMQ topic and sending the request to
  * Solr for indexing.
  *
  * Note: This is not production-ready code. This was written as demo code for presentation at CIRCUIT 2016.
+ *
+ * @author Gaston Gonzalez
  */
-public class AemToSolrRoutes
-{
-    public static void main( String[] args ) throws Exception
-    {
-        CamelContext camelContext = new DefaultCamelContext();
+public class AemToSolrRoutes {
 
+    private Main camelMain;
+
+    public static void main( String[] args ) throws Exception {
+        AemToSolrRoutes aemToSolrRoutes = new AemToSolrRoutes();
+        aemToSolrRoutes.boot();
+    }
+
+    private static class AemRouteToSolrRouteBuilder extends RouteBuilder {
+
+        public void configure() throws Exception {
+
+            from("activemq://topic:{{activemq.topic}}?clientId={{activemq.client.id}}&durableSubscriptionName={{activemq.durable.subscription.name}}")
+                .choice()
+                .when(simple("${body[aem.op]} == 'ADD_DOC'"))
+                    .to("direct:solrAdd")
+                .when(simple("${body[aem.op]} == 'DELETE_DOC'"))
+                    .to("direct:solrDelete")
+                .otherwise()
+                    .log("Unknown JMS operation")
+            .end()
+            ;
+
+            from("direct:solrAdd")
+                .process(new JmsDocToSolrDoc())
+                .setHeader(SolrConstants.OPERATION, constant(SolrConstants.OPERATION_INSERT))
+                .to("solrCloud://{{solr.host}}:{{solr.port}}/solr/{{solr.collection}}?zkHost={{solr.zkhost}}&collection={{solr.collection}}")
+                .to("direct:solrCommit")
+            ;
+
+            from("direct:solrDelete")
+                .process(new JmsDocToSolrDoc())
+                .setHeader(SolrConstants.OPERATION, constant(SolrConstants.OPERATION_DELETE_BY_ID))
+                .setBody(header(SolrConstants.FIELD + "id"))
+                .to("solrCloud://{{solr.host}}:{{solr.port}}/solr/{{solr.collection}}?zkHost={{solr.zkhost}}&collection={{solr.collection}}")
+                .to("direct:solrCommit")
+            ;
+
+            from("direct:solrCommit")
+                .setHeader(SolrConstants.OPERATION, constant(SolrConstants.OPERATION_COMMIT))
+                .to("solrCloud://{{solr.host}}:{{solr.port}}/solr/{{solr.collection}}?zkHost={{solr.zkhost}}&collection={{solr.collection}}")
+            ;
+        }
+    }
+
+    private void boot() throws Exception {
+
+        // Create main wrapper
+        camelMain = new Main();
+        CamelContext camelContext = camelMain.getOrCreateCamelContext();
+
+        // Initialize our properties file so that we can support property placeholder in our endpoint URIs
         PropertiesComponent propertiesComponent = camelContext.getComponent("properties", PropertiesComponent.class);
         propertiesComponent.setLocation("classpath:indexer.properties");
         propertiesComponent.setSystemPropertiesMode(PropertiesComponent.SYSTEM_PROPERTIES_MODE_OVERRIDE);
+        camelMain.bind("properties", propertiesComponent);
 
+        // Configure ActiveMQ
         ActiveMQComponent activemq = ActiveMQComponent.activeMQComponent(
                 PropertiesHelper.getProperty(PropertiesHelper.ACTIVEMQ_BROKER_URL_KEY, camelContext));
         activemq.setUserName(PropertiesHelper.getProperty(PropertiesHelper.ACTIVEMQ_USER_KEY, camelContext));
         activemq.setPassword(PropertiesHelper.getProperty(PropertiesHelper.ACTIVEMQ_PASS_KEY, camelContext));
         activemq.setTrustAllPackages(true);
-        camelContext.addComponent("activemq", activemq);
+        camelMain.bind("activemq", activemq);
 
-        camelContext.addRoutes(new RouteBuilder() {
-            @Override
-            public void configure() throws Exception
-            {
-                from("activemq://topic:{{activemq.topic}}?clientId={{activemq.client.id}}&durableSubscriptionName={{activemq.durable.subscription.name}}")
-                .choice()
-                    .when(simple("${body[aem.op]} == 'ADD_DOC'"))
-                        .to("direct:solrAdd")
-                    .when(simple("${body[aem.op]} == 'DELETE_DOC'"))
-                        .to("direct:solrDelete")
-                    .otherwise()
-                        .log("Unknown JMS operation")
-                .end()
-                ;
+        camelMain.addRouteBuilder(new AemRouteToSolrRouteBuilder());
 
-                from("direct:solrAdd")
-                    .process(new JmsDocToSolrDoc())
-                    .setHeader(SolrConstants.OPERATION, constant(SolrConstants.OPERATION_INSERT))
-                    .to("solrCloud://{{solr.host}}:{{solr.port}}/solr/{{solr.collection}}?zkHost={{solr.zkhost}}&collection={{solr.collection}}")
-                    .to("direct:solrCommit")
-                ;
-
-                // Set the document ID on the body
-                from("direct:solrDelete")
-                    .process(new JmsDocToSolrDoc())
-                    .setHeader(SolrConstants.OPERATION, constant(SolrConstants.OPERATION_DELETE_BY_ID))
-                    .setBody(header(SolrConstants.FIELD + "id"))
-                    .to("solrCloud://{{solr.host}}:{{solr.port}}/solr/{{solr.collection}}?zkHost={{solr.zkhost}}&collection={{solr.collection}}")
-                    .to("direct:solrCommit")
-                ;
-
-                from("direct:solrCommit")
-                    .setHeader(SolrConstants.OPERATION, constant(SolrConstants.OPERATION_COMMIT))
-                    .to("solrCloud://{{solr.host}}:{{solr.port}}/solr/{{solr.collection}}?zkHost={{solr.zkhost}}&collection={{solr.collection}}")
-                ;
-            }
-        });
-
-        camelContext.start();
-        Thread.sleep(1000 * 60 * 30); // 30 min
-        camelContext.stop();
+        camelMain.run();
     }
 }
